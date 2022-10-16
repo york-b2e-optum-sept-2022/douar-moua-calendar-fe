63d545b6d69bd01aee09d3d8589060c0af161f1d
douar <mouadouarzong@gmail.com>
2022-10-16 15:35:31 -0500

    unsubscribe get current user event list onNgDestory

--- src/app/invite.service.ts
+++ src/app/invite.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {HttpService} from "./http.service";
 import {IInvitations} from "./_interfaces/IInvitations";
-import {first, Subject} from "rxjs";
+import {first, Subject, Subscription} from "rxjs";
 import {EventService} from "./event.service";
 import {IEvents} from "./_interfaces/IEvents";
 import * as events from "events";
@@ -9,20 +9,21 @@ import * as events from "events";
 @Injectable({
   providedIn: 'root'
 })
-export class InviteService {
+export class InviteService implements OnDestroy {
 
   inviteList: IInvitations[] = []
   $inviteList = new Subject<IInvitations[]>()
 
   currentUserEventList: IEvents[] = []
+  currentUserEventListSub: Subscription;
 
   constructor(private httpService: HttpService, private eventService: EventService) {
 
     //get current user event list
-    this.eventService.$eventList.subscribe({
+    this.currentUserEventListSub = this.eventService.$eventList.subscribe({
       next: (eventList) => {
         this.currentUserEventList = eventList
-        console.log(this.currentUserEventList)
+        console.log(typeof this.currentUserEventList, this.currentUserEventList)
       },
       error: (err) => {
         console.error(err)
@@ -31,6 +32,7 @@ export class InviteService {
     })
   }
 
+  //get invites specific to event
   getInviteList(){
     this.httpService.getInviteList().pipe(first()).subscribe({
       next: (inviteList) => {
@@ -38,12 +40,16 @@ export class InviteService {
           return this.currentUserEventList.filter((event) => {return event.id = invite.eventID})
         })
         this.$inviteList.next(this.inviteList)
-        console.log(this.inviteList)
+        console.log(typeof this.inviteList, this.inviteList)
       },
       error: err => {
         console.error(err)
         alert('Unable to load list of invites. Please try again later.')
       }
     })
+  }
+
+  ngOnDestroy(){
+    this.currentUserEventListSub.unsubscribe()
   }
 }
