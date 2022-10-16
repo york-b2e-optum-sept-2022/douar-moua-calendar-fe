20f3b7bb93dc44a3c1322fe97c3c0776c18b713d
douar <mouadouarzong@gmail.com>
2022-10-16 06:11:03 -0500

    inject invite service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component,  } from '@angular/core';
 import {IAccounts} from "../_interfaces/IAccounts";
 import {AccountService} from "../account.service";
 import {EventService} from "../event.service";
+import {InviteService} from "../invite.service";
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,7 @@ export class LoginComponent {
     password: ""
   }
 
-  constructor(private accountService: AccountService, private eventService: EventService) { }
+  constructor(private accountService: AccountService, private eventService: EventService, private inviteService: InviteService) { }
 
   onLoginClick(){
     this.accountService.onLogin(this.loginAccount)
