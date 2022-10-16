a4a3fc91528dd6420318db5af679a3917d73465d
douar <mouadouarzong@gmail.com>
2022-10-16 06:07:23 -0500

    create getInviteList() retrieve invite data from getInvite() in httpService & save into local variable

--- src/app/invite.service.ts
+++ src/app/invite.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
 import {HttpService} from "./http.service";
+import {IEvents} from "./_interfaces/IEvents";
+import {IInvitations} from "./_interfaces/IInvitations";
+import {first} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InviteService {
 
+  inviteList: IInvitations[] = []
+
   constructor(private httpService: HttpService) { }
+
+  getInviteList(){
+    this.httpService.getInviteList().pipe(first()).subscribe({
+      next: (inviteList) => {
+        this.inviteList = inviteList
+      },
+      error: err => {
+        console.error(err)
+        alert('Unable to load list of invites. Please try again later.')
+      }
+    })
+  }
 }
