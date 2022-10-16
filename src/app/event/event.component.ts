470842ee1166fc8a929ac8256a6c748a7ee5f6d2
douar <mouadouarzong@gmail.com>
2022-10-16 06:33:37 -0500

    retrieve current user data from account service

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -4,6 +4,7 @@ import {EventService} from "../event.service";
 import {AccountService} from "../account.service";
 import {IAccounts} from "../_interfaces/IAccounts";
 import {InviteService} from "../invite.service";
+import {IInvitations} from "../_interfaces/IInvitations";
 
 @Component({
   selector: 'app-event',
@@ -16,21 +17,43 @@ export class EventComponent {
 
   isUpdating: boolean = false
 
-  accountList: IAccounts[] | null = null
+  // accountList: IAccounts[] | null = null
   // users!: IAccounts["username"][];
 
+  currentUser: IAccounts = {
+    id: '',
+    username: '',
+    password: ''
+  }
+
+  inviteList: IInvitations[] = []
+
   constructor(private eventService: EventService, private accountService: AccountService, private inviteSesrvice: InviteService) {
-    //get account list?
-    this.accountService.$accountList.subscribe({
-      next: accountList => {
-        this.accountList = accountList
-        // this.users = accountList.map(IAccount => IAccount.username)
+    //get account list TODO UNSUB
+    // this.accountService.$accountList.subscribe({
+    //   next: accountList => {
+    //     this.accountList = accountList
+    //     // this.users = accountList.map(IAccount => IAccount.username)
+    //   },
+    //   error: err => {
+    //     console.error(err)
+    //     alert('Unable to retrieve list of users. Please try again later.')
+    //   }
+    // })
+
+    //get current user data TODO UNSUB
+    this.accountService.$foundAccount.subscribe({
+      next: (currentUser) => {
+        this.currentUser = currentUser
       },
       error: err => {
         console.error(err)
-        alert('Unable to retrieve list of users. Please try again later.')
+        alert('Unable to find current user information. Please try again later.')
       }
     })
+
+    //get invite list
+
   }
 
   onDeleteClick(eventId: string){
