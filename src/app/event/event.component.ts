70b092a842b3e8b508074b909afb92b814095bd3
douar <mouadouarzong@gmail.com>
2022-10-16 15:28:15 -0500

    commented out unnecessary code added Input() invite data from parent comp event-list interpolate invite data to render

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -21,14 +21,15 @@ export class EventComponent {
   // accountList: IAccounts[] | null = null
   // users!: IAccounts["username"][];
 
-  currentUser: IAccounts = {
-    id: '',
-    username: '',
-    password: ''
-  }
+  // currentUser: IAccounts = {
+  //   id: '',
+  //   username: '',
+  //   password: ''
+  // }
 
-  inviteList: IInvitations[] = []
-  eventInviteList: IInvitations[] = []
+  // eventInviteList: IInvitations[] = []
+
+  @Input() invite!: IInvitations;
 
   constructor(private eventService: EventService, private accountService: AccountService, private inviteService: InviteService) {
     //get account list TODO UNSUB
@@ -43,32 +44,31 @@ export class EventComponent {
     //   }
     // })
 
-    //get current user data TODO UNSUB
-    this.accountService.$foundAccount.subscribe({
-      next: (currentUser) => {
-        this.currentUser = currentUser
-      },
-      error: err => {
-        console.error(err)
-        alert('Unable to find current user information. Please try again later.')
-      }
-    })
+    //get current user data TODO UNSUB DO I NEED THIS???
 
-    //get current user events invite list TODO UNSUB
-    this.inviteService.$inviteList.subscribe({
-      next: (inviteList) => {
-        this.inviteList = inviteList
-        this.eventInviteList = inviteList.filter((event) => {
-          return this.event.id == event.id
-        })
-        console.log(this.eventInviteList)
-        console.log(this.inviteList)
-      },
-      error: (err) => {
-        console.error(err)
-        alert('Unable to load list of invited users for event. Please try again later.')
-      }
-    })
+    // this.accountService.$foundAccount.subscribe({
+    //   next: (currentUser) => {
+    //     this.currentUser = currentUser
+    //     console.log(this.currentUser)
+    //   },
+    //   error: err => {
+    //     console.error(err)
+    //     alert('Unable to find current user information. Please try again later.')
+    //   }
+    // })
+
+    //get current users invite list specific to event TODO UNSUB
+
+    // this.inviteService.$inviteList.subscribe({
+    //   next: (inviteList) => {
+    //     this.eventInviteList = inviteList
+    //     console.log(this.eventInviteList)
+    //   },
+    //   error: (err) => {
+    //     console.error(err)
+    //     alert('Unable to load list of invited users for event. Please try again later.')
+    //   }
+    // })
 
   }
 
