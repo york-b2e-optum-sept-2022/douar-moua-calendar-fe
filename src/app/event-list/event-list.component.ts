de06c6c4ea7dc05b521dd71daff3bbed5f2ac8f4
douar <mouadouarzong@gmail.com>
2022-10-16 15:30:17 -0500

    get invite list from invite service give invite data to child comp event & render it

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnDestroy,} from '@angular/core';
 import {IEvents} from "../_interfaces/IEvents";
 import {EventService} from "../event.service";
 import {Subscription} from "rxjs";
+import {InviteService} from "../invite.service";
+import {IInvitations} from "../_interfaces/IInvitations";
 
 @Component({
   selector: 'app-event-list',
@@ -13,18 +15,33 @@ export class EventListComponent implements OnDestroy {
   eventsList: IEvents[] = []
   eventListSub: Subscription;
 
-  constructor(private eventService: EventService) {
+  eventInviteList: IInvitations[] = []
+
+
+  constructor(private eventService: EventService, private inviteService: InviteService) {
 
     // get event list from event service
     this.eventListSub = this.eventService.$eventList.subscribe({
       next: (eventList) => {
         this.eventsList = eventList
+        console.log(this.eventsList)
       },
       error: (err) => {
         console.error(err)
           alert('Unable to get list of events. Please try again later.')
       }
     });
+
+    this.inviteService.$inviteList.subscribe({
+      next: (inviteList) => {
+        this.eventInviteList = inviteList
+        console.log(this.eventInviteList)
+      },
+      error: (err) => {
+        console.error(err)
+        alert('Unable to load list of invited users for event. Please try again later.')
+      }
+    })
 
   }
 
