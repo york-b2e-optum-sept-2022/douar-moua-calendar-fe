89a4c77247f0fef040317b735eebb0893ff73b08
douar <mouadouarzong@gmail.com>
2022-10-16 04:24:58 -0500

    give account list data from create-event comp to child comp invite-list

---
+++ src/app/invite-list/invite-list.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IAccounts} from "../_interfaces/IAccounts";
+
+@Component({
+  selector: 'app-invite-list',
+  templateUrl: './invite-list.component.html',
+  styleUrls: ['./invite-list.component.css']
+})
+export class InviteListComponent implements OnInit {
+
+  @Input() accountList!: IAccounts;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
