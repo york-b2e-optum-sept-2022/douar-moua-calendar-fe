d99a30f93ef14798895abb81671940e75510285f
douar <mouadouarzong@gmail.com>
2022-10-16 17:52:07 -0500

    capture invite input and send data to invite service

--- src/app/invite-list/invite-list.component.ts
+++ src/app/invite-list/invite-list.component.ts
@@ -1,19 +1,28 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {IAccounts} from "../_interfaces/IAccounts";
+import {InviteService} from "../invite.service";
 
 @Component({
   selector: 'app-invite-list',
   templateUrl: './invite-list.component.html',
   styleUrls: ['./invite-list.component.css']
 })
-export class InviteListComponent implements OnInit {
+export class InviteListComponent {
 
   @Input() accountList!: IAccounts;
 
-  constructor() {
+  selectedUser: IAccounts = {
+    id: '',
+    username: '',
+    password: ''
   }
 
-  ngOnInit(): void {
+  constructor(private inviteService: InviteService) {
   }
+
+  onInviteUserCheck(){
+    this.inviteService.createInvite()
+  }
+
 
 }
