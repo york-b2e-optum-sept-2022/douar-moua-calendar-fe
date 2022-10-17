814b540079572897f30d8760a6295e1b83df5021
douar <mouadouarzong@gmail.com>
2022-10-17 00:28:58 -0500

    cleaned up code and added comments

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,9 +1,6 @@
 import {Component, Input,} from '@angular/core';
 import {IEvents} from "../_interfaces/IEvents";
 import {EventService} from "../event.service";
-import {AccountService} from "../account.service";
-import {IAccounts} from "../_interfaces/IAccounts";
-import {InviteService} from "../invite.service";
 import {IInvitations} from "../_interfaces/IInvitations";
 
 @Component({
@@ -13,59 +10,14 @@ import {IInvitations} from "../_interfaces/IInvitations";
 })
 export class EventComponent {
 
-  //get events as individual events
+  //get events from event-list parent comp
   @Input() event!: IEvents;
 
   isUpdating: boolean = false
 
-  // accountList: IAccounts[] | null = null
-  // users!: IAccounts["username"][];
-  // currentUser: IAccounts = {
-  //   id: '',
-  //   username: '',
-  //   password: ''
-  // }
-  // eventInviteList: IInvitations[] = []
-
   @Input() invite!: IInvitations[];
 
-  constructor(private eventService: EventService, private accountService: AccountService, private inviteService: InviteService) {
-    //get account list
-    // this.accountService.$accountList.subscribe({
-    //   next: accountList => {
-    //     this.accountList = accountList
-    //     // this.users = accountList.map(IAccount => IAccount.username)
-    //   },
-    //   error: err => {
-    //     console.error(err)
-    //     alert('Unable to retrieve list of users. Please try again later.')
-    //   }
-    // })
-
-    //get current user data
-    // this.accountService.$foundAccount.subscribe({
-    //   next: (currentUser) => {
-    //     this.currentUser = currentUser
-    //     console.log(this.currentUser)
-    //   },
-    //   error: err => {
-    //     console.error(err)
-    //     alert('Unable to find current user information. Please try again later.')
-    //   }
-    // })
-
-    //get current users invite list specific to event
-    // this.inviteService.$inviteList.subscribe({
-    //   next: (inviteList) => {
-    //     this.eventInviteList = inviteList
-    //     console.log(this.eventInviteList)
-    //   },
-    //   error: (err) => {
-    //     console.error(err)
-    //     alert('Unable to load list of invited users for event. Please try again later.')
-    //   }
-    // })
-
+  constructor(private eventService: EventService) {
   }
 
   onDeleteClick(eventId: string){
